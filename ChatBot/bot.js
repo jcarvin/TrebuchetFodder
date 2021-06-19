93b113b38ff26db840dd5fc2e70ef5d39218074e
jcarvin <j.d.carvin@gmail.com>
2021-06-19 16:01:00 -0400

    some websocket cleanup. Need to scope the chatbot into the websocket so that it can send messages to the front end

--- ChatBot/bot.js
+++ ChatBot/bot.js
@@ -13,14 +13,14 @@ const opts = {
 };
 
 // Create a client with our options
-const client = new tmi.client(opts);
+const twitchClient = new tmi.client(opts);
 
 // Register our event handlers (defined below)
-client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
+twitchClient.on('message', onMessageHandler);
+twitchClient.on('connected', onConnectedHandler);
 
 // Connect to Twitch:
-client.connect();
+twitchClient.connect();
 
 // Called every time a message comes in
 function onMessageHandler(channel, context, msg, self) {
@@ -37,17 +37,17 @@ function onMessageHandler(channel, context, msg, self) {
   if (/!d\d/.test(commandName)) {
     // !d20 | !d999999999 | pretty much anything !d{number}
     const num = rollDice(commandName);
-    client.say(channel, `You rolled a ${num}.`);
+    twitchClient.say(channel, `You rolled a ${num}.`);
     console.log(`* Executed ${commandName} command`);
   } else if (commandName === '!msg') {
     // !msg {channelName} {message}
     const channelName = messageArray[1];
     const message = messageArray.slice(2, messageArray.length).join(' ');
     // send the message to the specified channel
-    client.say(channelName, `From ${channel}'s chat: "${message}"`);
+    twitchClient.say(channelName, `From ${channel}'s chat: "${message}"`);
     console.log(`From ${channel}'s chat: "${message}" To: ${channelName}`);
     // let the host channel know that the message has been sent
-    client.say(
+    twitchClient.say(
       channel,
       `Your message has been sent to ${channelName}'s chat! `
     );
@@ -69,7 +69,7 @@ function onConnectedHandler(addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
 }
 
-// ----------- websocket
+// ---------------------- websocket ----------------------
 const webSocketsServerPort = 8082;
 const webSocketServer = require('websocket').server;
 const http = require('http');
@@ -79,10 +79,11 @@ server.listen(webSocketsServerPort);
 const wsServer = new webSocketServer({
   httpServer: server,
 });
-// I'm maintaining all active connections in this object
+// We're maintaining all active connections in this object.
+// honestly, it's probably unnecessary right now bug *shrug*
 const clients = {};
 
-// This code generates unique userid for everyuser.
+// This code generates unique userid for every user.
 const getUniqueID = () => {
   const s4 = () =>
     Math.floor((1 + Math.random()) * 0x10000)
@@ -95,25 +96,20 @@ wsServer.on('request', function (request) {
   var userID = getUniqueID();
   console.log(
     new Date() +
-      ' Recieved a new connection from origin ' +
+      ' Received a new connection from origin ' +
       request.origin +
       '.'
   );
-  // You can rewrite this part of the code to accept only the requests from allowed origin
   const connection = request.accept(null, request.origin);
   clients[userID] = connection;
   console.log(
     'connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients)
   );
-  connection.send('hey');
-  console.log('sent message');
-});
 
-wsServer.on('connect', (socket) => {
-  socket.on('message', function incoming(data) {
+  connection.on('message', function incoming(data) {
     console.log(data);
     // if you want to send that message back to the client who sent it,
     // you can use send method on the socket
-    socket.send(data);
+    connection.send(JSON.stringify(data));
   });
 });
--- TrebuchetFodder/src/components/Websocket/WebsocketConnector.js
+++ TrebuchetFodder/src/components/Websocket/WebsocketConnector.js
@@ -5,21 +5,20 @@ import PropTypes from 'prop-types';
 import { w3cwebsocket as W3CWebSocket } from 'websocket';
 
 const client = new W3CWebSocket('ws://127.0.0.1:8082');
+client.onopen = () => {
+  console.log('WebSocket Client Connected');
+};
+client.onerror = (error) => {
+  console.log(`WebSocket error: ${error}`);
+};
+client.onmessage = (message) => {
+  console.log('websocket message: ', message, JSON.parse(message.data));
+};
 
 const WebsocketConnector = ({}) => {
   useEffect(() => {
-    console.log('mounted?', client);
-    client.send('anything?');
-    client.onopen = () => {
-      console.log('WebSocket Client Connected!! woooohooooo');
-    };
-    client.onerror = (error) => {
-      console.log(`WebSocket error: ${error}`);
-    };
-    client.onmessage = (message) => {
-      console.log('websocket message: ', message);
-    };
-  }, [client]);
+    client.send('Component has mounted');
+  }, []);
 
   return <Fragment>WebSocket Time</Fragment>;
 };
